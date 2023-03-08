f51e7377770c4d5d085dbd978e53a8cc3ab11bbe
Jordan Kowal <jkowal@idmog.com>
2023-03-08 17:33:52 +0100

    django: Added improved manager

--- jklib/dj/managers.py
+++ jklib/dj/managers.py
@@ -2,21 +2,42 @@
 from typing import Iterable, List, Optional, Sequence
 
 # Django
-from django.db import models
+from django.db.models import Manager, Model, QuerySet
 
 
-class NoBulkManager(models.Manager):
-    """Disables bulk operations on a model."""
+class ImprovedManager(Manager):
+    def __init__(
+        self,
+        allow_bulk: bool = True,
+        select_related: Optional[Iterable[str]] = None,
+        prefetch_related: Optional[Iterable[str]] = None,
+    ):
+        super().__init__()
+        self.allow_bulk = allow_bulk
+        self.select_related = select_related or []
+        self.prefetch_related = prefetch_related or []
 
     def bulk_create(
         self,
         objs: Iterable,
-        batch_size: Optional[int] = ...,
-        ignore_conflicts: bool = ...,
+        batch_size: Optional[int] = None,
+        ignore_conflicts: bool = False,
     ) -> List:
-        raise NotImplementedError
+        if not self.allow_bulk:
+            raise NotImplementedError
+        return super().bulk_create(objs, batch_size, ignore_conflicts)
 
     def bulk_update(
-        self, objs: Iterable, fields: Sequence[str], batch_size: Optional[int] = ...
+        self, objs: Iterable, fields: Sequence[str], batch_size: Optional[int] = None
     ) -> int:
-        raise NotImplementedError
+        if not self.allow_bulk:
+            raise NotImplementedError
+        return super().bulk_update(objs, fields, batch_size)
+
+    def get_queryset(self) -> QuerySet["Model"]:
+        queryset = super().get_queryset()
+        if self.select_related:
+            queryset = queryset.select_related(*self.select_related)
+        if self.prefetch_related:
+            queryset = queryset.prefetch_related(*self.prefetch_related)
+        return queryset
